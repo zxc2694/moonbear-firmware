26bb327eb18f97c25f04020a369b8a9e4933033a
fboris <fantasyboris@gmail.com>
2013-12-06 18:54:26 +0800

    Disable TIM8 CH1 and CH2 PWM output

--- Program/Program_Module/module_motor.c
+++ Program/Program_Module/module_motor.c
@@ -17,9 +17,9 @@ void Motor_Config( void )
   TIM_TimeBaseInitTypeDef TIM_TimeBaseStruct;
   TIM_OCInitTypeDef TIM_OCInitStruct;
 
-  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA | RCC_AHB1Periph_GPIOB | RCC_AHB1Periph_GPIOC, ENABLE);
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA | RCC_AHB1Periph_GPIOB, ENABLE);
   RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3 , ENABLE);
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM8, ENABLE);
+
 
 
 
@@ -27,8 +27,6 @@ void Motor_Config( void )
   GPIO_PinAFConfig(GPIOA, GPIO_PinSource7, GPIO_AF_TIM3);
   GPIO_PinAFConfig(GPIOB, GPIO_PinSource0, GPIO_AF_TIM3);
   GPIO_PinAFConfig(GPIOB, GPIO_PinSource1, GPIO_AF_TIM3);
-  GPIO_PinAFConfig(GPIOC, GPIO_PinSource6, GPIO_AF_TIM8);
-  GPIO_PinAFConfig(GPIOC, GPIO_PinSource7, GPIO_AF_TIM8);
 
   /* TIM2 PWM3  PA0 */	/* TIM2 PWM4  PA1 */	/* TIM2 PWM5  PA2 */	/* TIM2 PWM8  PA3 */
   /* TIM3 PWM9  PA6 */	/* TIM3 PWM10 PA7 */
@@ -45,17 +43,11 @@ void Motor_Config( void )
   GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
   GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
   GPIO_Init(GPIOB, &GPIO_InitStruct);
-  /* TIM8 PWM7  PC6 */	/* TIM8 PWM6  PC7 */
-  GPIO_InitStruct.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;
-  GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
-  GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
-  GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
-  GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
-  GPIO_Init(GPIOC, &GPIO_InitStruct);
+
 
 
   TIM_DeInit(TIM3);
-  TIM_DeInit(TIM8);
+
 
   /************************** PWM Output **************************************/
   /* 設定 TIM2 TIM3 TIM4 Time Base */
@@ -64,12 +56,6 @@ void Motor_Config( void )
   TIM_TimeBaseStruct.TIM_ClockDivision = TIM_CKD_DIV1;
   TIM_TimeBaseStruct.TIM_CounterMode = TIM_CounterMode_Up;    // 上數
   TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStruct);
-  /* 設定TIM8 Time Base */
-  TIM_TimeBaseStruct.TIM_Period = (u16)(2500-1);              // 週期 = 2.5ms, 400kHz
-  TIM_TimeBaseStruct.TIM_Prescaler = (u16)(168-1);            // 除頻42 = 1M ( 1us )
-  TIM_TimeBaseStruct.TIM_ClockDivision = TIM_CKD_DIV1;
-  TIM_TimeBaseStruct.TIM_CounterMode = TIM_CounterMode_Up;    // 上數
-  TIM_TimeBaseInit(TIM8, &TIM_TimeBaseStruct);
 
   /* 設定 TIM2 TIM3 TIM4 TIM8 OC */
   TIM_OCInitStruct.TIM_OCMode = TIM_OCMode_PWM1;              // 配置為 PWM1 模式
@@ -80,20 +66,14 @@ void Motor_Config( void )
   TIM_OC2Init(TIM3, &TIM_OCInitStruct);                       // 初始化 TIM3 OC2
   TIM_OC3Init(TIM3, &TIM_OCInitStruct);                       // 初始化 TIM3 OC3
   TIM_OC4Init(TIM3, &TIM_OCInitStruct);                       // 初始化 TIM3 OC4
-  TIM_OC1Init(TIM8, &TIM_OCInitStruct);                       // 初始化 TIM8 OC1
-  TIM_OC2Init(TIM8, &TIM_OCInitStruct);                       // 初始化 TIM8 OC2
   TIM_OC1PreloadConfig(TIM3, TIM_OCPreload_Enable);           // 致能 TIM3 OC1 預裝載
   TIM_OC2PreloadConfig(TIM3, TIM_OCPreload_Enable);           // 致能 TIM3 OC2 預裝載
   TIM_OC3PreloadConfig(TIM3, TIM_OCPreload_Enable);           // 致能 TIM3 OC3 預裝載
   TIM_OC4PreloadConfig(TIM3, TIM_OCPreload_Enable);           // 致能 TIM3 OC4 預裝載
-  TIM_OC1PreloadConfig(TIM8, TIM_OCPreload_Enable);           // 致能 TIM8 OC1 預裝載
-  TIM_OC2PreloadConfig(TIM8, TIM_OCPreload_Enable);           // 致能 TIM8 OC2 預裝載
 
   /* 啟動 */
   TIM_ARRPreloadConfig(TIM3, ENABLE);                         // 致能 TIM3 重載寄存器ARR
-  TIM_ARRPreloadConfig(TIM8, ENABLE);                         // 致能 TIM8 重載寄存器ARR
   TIM_Cmd(TIM3, ENABLE);                                      // 致能 TIM3
-  TIM_Cmd(TIM8, ENABLE);                                      // 致能 TIM8
 
   PWM_Motor1 = PWM_MOTOR_MIN;
   PWM_Motor2 = PWM_MOTOR_MIN;
