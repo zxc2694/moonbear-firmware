445e6413c3158eb206f7d308f3bf9498d6495021
fboris <fantasyboris@gmail.com>
2013-12-10 20:13:29 +0800

    Add new typedef to deal with global variable

---
+++ Program/Program_System/sys_manager.h
@@ -0,0 +1,34 @@
+
+#ifndef __SYS_MANAGER_H
+#define __SYS_MANAGER_H
+
+#include "stm32f4xx.h"
+enum 
+{
+	PWM1_CCR=0,
+	PWM2_CCR,
+	PWM3_CCR,
+	PWM4_CCR,
+	PWM5_CCR,
+	RC_EXP_ROLL,
+	RC_EXP_PITCH,
+	RC_EXP_YAW,
+	RC_EXP_THR,
+	TRUE_ROLL,
+	TRUE_PITCH,
+	TRUE_YAW,
+	GLOABAL_PARAM_COUNT
+};
+
+
+typedef struct global_struct
+{
+	
+	char *param_name;
+	float param;
+
+}global_struct;
+
+
+
+#endif
