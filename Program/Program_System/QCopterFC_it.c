44d22265f17a73ae0d9ce19fda5a621b926f7a0c
Shengwen1997 <l1996812@gmail.com>
2013-12-13 00:16:00 +0800

    Remove the useless handler function

--- Program/Program_System/QCopterFC_it.c
+++ Program/Program_System/QCopterFC_it.c
@@ -557,15 +557,7 @@ void UsageFault_Handler(void)
 {
 	while (1);
 }
-void SVC_Handler(void)
-{
-	while (1);
-}
 void DebugMon_Handler(void)
-{
-	while (1);
-}
-void PendSV_Handler(void)
 {
 	while (1);
 }
