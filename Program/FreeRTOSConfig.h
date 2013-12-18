05471955f3933e1a6bf0b2156a5f7aaf2aa72676
fboris <fantasyboris@gmail.com>
2013-12-18 16:04:22 +0800

    Change task name length and MAX_PRIORITIES to 10

--- Program/FreeRTOSConfig.h
+++ Program/FreeRTOSConfig.h
@@ -86,10 +86,10 @@
 #define configUSE_TICK_HOOK                             1
 #define configCPU_CLOCK_HZ				(168000000)
 #define configTICK_RATE_HZ				( ( portTickType ) 1000 )
-#define configMAX_PRIORITIES			( ( unsigned portBASE_TYPE ) 5 )
+#define configMAX_PRIORITIES			( ( unsigned portBASE_TYPE ) 10 )
 #define configMINIMAL_STACK_SIZE		( ( unsigned short ) 130 )
 #define configTOTAL_HEAP_SIZE			( ( size_t ) ( 75 * 1024 ) )
-#define configMAX_TASK_NAME_LEN			( 10 )
+#define configMAX_TASK_NAME_LEN			( 20 )
 #define configUSE_TRACE_FACILITY		1
 #define configUSE_16_BIT_TICKS			0
 #define configIDLE_SHOULD_YIELD			1
