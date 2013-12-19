ae107bb1a6562c944cfeff22a81cb0a69a364d15
zxc2694 <zxc2694zxc2694@yahoo.com.tw>
2013-12-19 11:09:49 +0800

    Delete useless code in the module_rs232.c, and delete test_TXRX.

--- Program/Program_Module/module_rs232.h
+++ Program/Program_Module/module_rs232.h
@@ -16,14 +16,6 @@ typedef struct{
 } serial_msg;
 
 void RS232_Config(void);
-void RS232_DMA_Config(void);
-void RS232_SendStr(USART_TypeDef *USARTx, char *pWord);
-void RS232_SendNum(USART_TypeDef *USARTx, u8 Type, u8 NumLen, s32 SendData);
-int RS232_SendData(USART_TypeDef *USARTx, char *SendData, u16 DataLen);
-void RS232_RecvStr(USART_TypeDef *USARTx, char *pWord);
-void RS232_RecvData(USART_TypeDef *USARTx, u8 *RecvData, u16 DataLen);
-void RS232_VisualScope(USART_TypeDef *, u8 *, u16);
-uint16_t getch_base(void);
 void putch_base(char str);
 int printf(const char *format, ...);
 int puts(const char* msg);
