e8278bca2a92cbe3abf3013812997a4d28067bb7
fboris <fantasyboris@gmail.com>
2013-12-20 22:53:50 +0800

    Store the value of Final_M1~M4 before bonding
    Because I want to see the "real" value of the parameter.
    Even if it is out of the range, we still need to see the
    real value of it.

--- Program/Program_System/QCopterFC_it.c
+++ Program/Program_System/QCopterFC_it.c
@@ -317,21 +317,24 @@ void SysTick_Handler(void)
 		global_var[PID_PITCH].param = Pitch;
 		global_var[PID_YAW].param = Yaw;
 
+
 		/* Motor Ctrl */
 		Final_M1 = Thr + Pitch - Roll - Yaw;
 		Final_M2 = Thr + Pitch + Roll + Yaw;
 		Final_M3 = Thr - Pitch + Roll - Yaw;
 		Final_M4 = Thr - Pitch - Roll + Yaw;
+		
+		global_var[MOTOR1].param = Final_M1;
+		global_var[MOTOR2].param = Final_M2;
+		global_var[MOTOR3].param = Final_M3;
+		global_var[MOTOR4].param = Final_M4;
 
 		Bound(Final_M1, PWM_MOTOR_MIN, PWM_MOTOR_MAX);
 		Bound(Final_M2, PWM_MOTOR_MIN, PWM_MOTOR_MAX);
 		Bound(Final_M3, PWM_MOTOR_MIN, PWM_MOTOR_MAX);
 		Bound(Final_M4, PWM_MOTOR_MIN, PWM_MOTOR_MAX);
 
-		global_var[MOTOR1].param = Final_M1;
-		global_var[MOTOR2].param = Final_M2;
-		global_var[MOTOR3].param = Final_M3;
-		global_var[MOTOR4].param = Final_M4;
+
 		/* Check Connection */
 #define NoSignal 1  // 1 sec
 		if ( safety == 1 ){
