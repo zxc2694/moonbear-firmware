e4aba4e582baff22bfcd01ad1a7007416143e3f2
Shengwen1997 <l1996812@gmail.com>
2013-12-22 13:08:38 +0800

    Add some C Std functions that using the memory operating

--- Program/string.c
+++ Program/string.c
@@ -2,6 +2,8 @@
 #include <stdint.h>
 #include <limits.h>
 
+#include "string.h"
+
 #define ALIGN (sizeof(size_t))
 #define ONES ((size_t)-1/UCHAR_MAX)                                                                      
 #define HIGHS (ONES * (UCHAR_MAX/2+1))
@@ -17,16 +19,16 @@ char *strchr(const char *s, int c)
 
 char *strcpy(char *dest, const char *src)
 {
-        const unsigned char *s = src;
-        unsigned char *d = dest;
+        const  char *s = src;
+        char *d = dest;
         while ((*d++ = *s++));
         return dest;
 }
 
 char *strncpy(char *dest, const char *src, size_t n)
 {
-        const unsigned char *s = src;
-        unsigned char *d = dest;
+        const char *s = src;
+        char *d = dest;
         while (n-- && (*d++ = *s++));
         return dest;
 }
@@ -62,4 +64,88 @@ char *strcat(char* dst, char* src)
         dst--;
         while (*dst++ = *src++);
         return retAddr;
+}
+
+char *strdup(const char *str)
+{
+        char *ptr;
+
+        ptr = (char *)pvPortMalloc(strlen(str) + 1);
+        if(ptr == NULL)
+                return NULL;
+
+        /*make the end of string have null character*/
+        ptr[strlen(str)+1] = '\0';
+        return memcpy(ptr, str, strlen(str));
+}
+
+void *memset(void *dest, int c, size_t n)
+{
+        unsigned char *s = dest;
+        c = (unsigned char)c;
+        for (; ((uintptr_t)s & ALIGN) && n; n--) *s++ = c;
+        if (n) {
+                size_t *w, k = ONES * c;
+                for (w = (void *)s; n>=SS; n-=SS, w++) *w = k;
+                for (s = (void *)w; n; n--, s++) *s = c;
+        }
+        return dest;
+}
+
+void *memcpy(void *dest, const void *src, size_t n)
+{
+        void *ret = dest;
+
+        //Cut rear
+        uint8_t *dst8 = dest;
+        const uint8_t *src8 = src;
+        switch (n % 4) {
+                case 3 : *dst8++ = *src8++;
+                case 2 : *dst8++ = *src8++;
+                case 1 : *dst8++ = *src8++;
+                case 0 : ;
+        }
+
+        //stm32 data bus width
+        uint32_t *dst32 = (void *)dst8;
+        const uint32_t *src32 = (void *)src8;
+        n = n / 4;
+        while (n--) {
+                *dst32++ = *src32++;
+        }
+
+        return ret;
+}
+
+void *memmove(void *dest, const void *src, size_t count)
+{
+        char *tmp;
+        const char *s;
+
+        if(dest <= src) {
+                tmp = dest;
+                s = src;
+                while(count--)
+                        *tmp++ = *s++;
+        } else {
+                tmp = dest;
+                tmp += count;
+                s= src;
+                s += count;
+                while(count--)
+                        *--tmp = *--s;
+        }
+        return dest;
+}
+
+int memcmp(const void *cs, const void *ct, unsigned long count)
+{
+	const unsigned char *su1, *su2;
+	int res = 0;
+ 
+	for (su1=cs, su2=ct; 0<count; ++su1, ++su2, count--)
+		if ((res = *su1 - *su2) != 0)
+			break;
+ 
+	return res;
 }
--- Program/string.h
+++ Program/string.h
@@ -9,5 +9,11 @@ char *strncpy(char *dest, const char *src, size_t n);
 size_t strlen(const char *s);
 int strcmp(const char* s1, const char* s2);
 char *strcat(char* dst, char* src);
+char *strdup(const char *str);
+
+void *memset(void *dest, int c, size_t n);
+void *memcpy(void *dest, const void *src, size_t n);
+void *memmove(void *dest, const void *src, size_t count);
+int memcmp(const void *cs, const void *ct, unsigned long count);
 
 #endif
