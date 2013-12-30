a6be87d1d4643d640c2607566998f7b2fefd481d
Shengwen1997 <l1996812@gmail.com>
2013-12-31 01:34:48 +0800

    Move the Status Monitor out to be an independent program
    *Also start using command parser into the program

--- Program/shell.c
+++ Program/shell.c
@@ -14,6 +14,8 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
+#include "status_monitor.h"
+
 int completion_disable = 0;
 int history_disable = 0;
 
@@ -93,109 +95,4 @@ void shell_ps(char parameter[][MAX_CMD_LEN], int par_cnt)
 	printf("*****************************************\n\r");
 	printf("%s", buf);
 }
-
-
-enum MONITOR_INTERNAL_CMD {
-	MONITOR_UNKNOWN,
-	MONITOR_QUIT,
-	MONITOR_RESUME,
-	MONITOR_CMD_CNT
-};
-
-char monitor_cmd[MONITOR_CMD_CNT - 1][MAX_CMD_LEN] = {"quit", "resume"};
-
-int monitorInternalCmdIndentify(char *command)
-{
-	int i;
-	for(i = 0; i < (MONITOR_CMD_CNT - 1); i++) {
-		if(strcmp(command ,monitor_cmd[i]) == 0) {
-			return i + 1;
-		}
-	}
-
-	return MONITOR_UNKNOWN;
-}
-
-void shell_monitor(char parameter[][MAX_CMD_LEN], int par_cnt)
-{
-	while(1) {
-		/* Clean the screen */
-		printf("\x1b[H\x1b[2J");
-
-		/* Welcome Messages */
-		printf("QuadCopter Status Monitor\n\r");
-		printf("Copyleft - NCKU Open Source Work of 2013 Embedded system class\n\r");
-		printf("**************************************************************\n\r");
-
-		printf("PID Parameters\n\r");
-		printf("Kp \t: %d\n\rKi\t: %d\n\rKd\t: %d\n\r", 0, 0, 0);
-
-		printf("--------------------------------------------------------------\n\r");
-
-		printf("Copter Attitudes <true value>\n\r");
-		printf("Pitch\t: %d\n\rRoll\t: %d\n\rYaw\t: %d\n\r", AngE.Pitch, AngE.Roll, AngE.Yaw);
-
-		printf("--------------------------------------------------------------\n\r");
-
-		#define MOTOR_STATUS "Off"
-		printf("Radio Control Messages\n\r");
-		printf("Pitch(expect)\t: %d\n\rRoll(expect)\t: %d\n\rYaw(expect)\t: %d\n\r", 
-			global_var[RC_EXP_PITCH].param, global_var[RC_EXP_ROLL].param, 
-			global_var[RC_EXP_YAW].param);
-
-		printf("Throttle\t: %d\n\r", global_var[RC_EXP_THR].param);
-		printf("Engine\t\t: %s\n\r", MOTOR_STATUS);
-
-		printf("--------------------------------------------------------------\n\r");
-
-		#define LED_STATUS "Off"
-		printf("LED lights\n\r");
-		printf("LED1\t: %s\n\rLED4\t: %s\n\rLED3\t: %s\n\rLED4\t: %s\n\r", LED_STATUS, LED_STATUS, LED_STATUS, LED_STATUS);
-
-		printf("**************************************************************\n\r\n\r");
-
-		printf("[Please press <Space> to refresh the status]\n\r");
-		printf("[Please press <Enter> to enable the command line]");
-	
-		int monitor_cmd = 0;		
-		char key_pressed = serial.getch();
-
-		while(monitor_cmd != MONITOR_QUIT && monitor_cmd != MONITOR_RESUME) {
-			if(key_pressed == ENTER) {
-				/* Clean and move up two lines*/
-				printf("\x1b[0G\x1b[0K\x1b[0A\x1b[0G\x1b[0K");
-	
-				while(monitor_cmd != MONITOR_QUIT && monitor_cmd != MONITOR_RESUME) {
-					char *command_str;
-
-					command_str = linenoise("(monitor) ");
-				
-					/* Check if it is internal command */
-					/* The Internal command means that need not call
-					   any functions but handle at here
-					 */
-					monitor_cmd = monitorInternalCmdIndentify(command_str);
-
-					/* Check if it is not an Internal command */
-					if(monitor_cmd == MONITOR_UNKNOWN) {
-						/* FIXME:External Commands, need to call other functions */
-					}
-
-					printf("\x1b[0A");
-				}
-			} else if(key_pressed == SPACE) {
-				break;
-			} else {
-				key_pressed = serial.getch();
-			}			
-		}
-
-		if(monitor_cmd == MONITOR_QUIT)
-			break;
-	}
-
-	/* Clean the screen */
-        printf("\x1b[H\x1b[2J");
-}
-
 
---
+++ Program/status_monitor.h
@@ -0,0 +1,8 @@
+#ifndef __STATUS_MONITOR_H
+#define __STATUS_MONITOR_H
+
+#include "parser.h"
+
+void shell_monitor(char parameter[][MAX_CMD_LEN], int par_cnt);
+
+#endif
