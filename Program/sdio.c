79579af0d596a1eac4ce1ad73073e653961e10cf
zxc2694 <zxc2694zxc2694@yahoo.com.tw>
2014-01-03 18:17:17 +0800

    Delete useless code in the sdio.c

--- Program/sdio.c
+++ Program/sdio.c
@@ -36,103 +36,15 @@ int SD_demo(void)
 	uint8_t TrData[8][32] = {0};
 	uint8_t WriteData[50] = "ABCDEFG";
 
-
-	// SD Card Init Info
-	printf(" \r\n");
-	printf("----------------------\r\n");
-	printf("----------------------\r\n");
-	printf(" SDIO SD FatFs demo\r\n");
-	printf("----------------------\r\n");
-	printf("----------------------\r\n\r\n");
-	printf(" SD Init ... ");
-
-	printf("OK!!\r\n\r\n");
-
-	printf("-----SD Init Info-----\r\n");
-
-	printf(" Capacity : ");
-	printf("%d MB\r\n", SDCardInfo.CardCapacity >> 20);
-
-	printf(" CardBlockSize : ");
-	printf("%d\r\n", SDCardInfo.CardBlockSize);
-
-	printf(" CardType : ");
-	printf("%d\r\n", SDCardInfo.CardType);
-
-	printf(" RCA : ");
-	printf("%d\r\n", SDCardInfo.RCA);
-	printf("----------------------\r\n\r\n");
-
-	/*  // Wait
-	  while(KEY != 1){
-	    LED_G = ~LED_G;
-	    vTaskDelay(200);
-	  }*/
-
-	// Read Directory File
-	printf("----------------------\r\n");
-	printf(" SD_Card Read Directory File\r\n");
-	printf("----------------------\r\n\r\n");
-
 	res = f_mount(&FatFs, "", 1);
 	res = f_opendir(&dirs, "0:/");
 	res = f_readdir(&dirs, &finfo);
-
-	while (res != FR_OK) {
-		printf(" FatFs failed!!\r\n");
-
-		while (1) {
-			vTaskDelay(200);
-		}
-	}
-
-	printf(" File name : \r\n");
-
-	while (finfo.fname[0]) {
-		f_readdir(&dirs, &finfo);
-
-		if (!finfo.fname[0])
-			break;
-
-		printf(" ... ");
-		printf("%s\r\n", finfo.fname);
-	}
-
-	/*  // Wait
-	  while(KEY != 1){
-	    LED_G = ~LED_G;
-	    vTaskDelay(200);
-	  }*/
-
-	// Write File
-	printf(" f_open ... ");
 	res = f_open(&file, "SDCard_K.txt", FA_OPEN_ALWAYS | FA_READ | FA_WRITE);
-
-	if (res == FR_OK)
-		printf("OK!!\r\n");
-	else
-		printf("failed!!\r\n");
-
-	printf(" f_write ... ");
 	res = f_write(&file, WriteData, 20, (UINT *)&i);
-
-	if (res == FR_OK)
-		printf("OK!!\r\n");
-	else
-		printf("failed!!\r\n");
-
 	file.fptr = 0;
-
-	printf(" f_read ... ");
 	res = f_read(&file, ReadBuf, 100, (UINT *)&i);
 
-	if (res == FR_OK)
-		printf("OK!!\r\n");
-	else
-		printf("failed!!\r\n");
-
 	f_close(&file);
-
 	printf("%s", ReadBuf);
 
 	while (1) {
