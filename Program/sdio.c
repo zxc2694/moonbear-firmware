b155c1ff1cfb19d79b8aca732a903ae9ba050c8a
zxc2694 <zxc2694zxc2694@yahoo.com.tw>
2014-01-03 20:31:58 +0800

    let kp & kd parameter can store in SD card.

--- Program/sdio.c
+++ Program/sdio.c
@@ -33,19 +33,22 @@ int SD_demo(void)
 {
 	uint32_t i = 0;
 	uint8_t ReadBuf[128] = {0};
-	uint8_t TrData[8][32] = {0};
-	uint8_t WriteData[50] = "ABCDEFG";
+	char WriteData[50];
 
+	float Kp = 12.123;
+	float Kd = 50.966;
+	sprintf(WriteData,"Kp : %f , Kd : %f  ",Kp,Kd);
+	printf("%s\n", WriteData);
 	res = f_mount(&FatFs, "", 1);
 	res = f_opendir(&dirs, "0:/");
 	res = f_readdir(&dirs, &finfo);
 	res = f_open(&file, "SDCard_K.txt", FA_OPEN_ALWAYS | FA_READ | FA_WRITE);
-	res = f_write(&file, WriteData, 20, (UINT *)&i);
+	res = f_write(&file, WriteData, 40, (UINT *)&i);
 	file.fptr = 0;
 	res = f_read(&file, ReadBuf, 100, (UINT *)&i);
 
 	f_close(&file);
-	printf("%s", ReadBuf);
+	printf("%s\n", ReadBuf);
 
 	while (1) {
 		vTaskDelay(200);
