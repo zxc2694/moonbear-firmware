c68ce36675fefb72dc305384cedea24ab342d56a
zxc2694 <zxc2694zxc2694@yahoo.com.tw>
2014-01-04 19:27:03 +0800

    Using strlen replace direct size of display.

--- Program/sdio.c
+++ Program/sdio.c
@@ -40,14 +40,13 @@ void sdio_task()
 	float Kd = 50.966;
 
 	sprintf(WriteData,"Kp : %f , Kd : %f  ",Kp,Kd);
-	printf("%s\n", WriteData);
 	res = f_mount(&FatFs, "", 1);
 	res = f_opendir(&dirs, "0:/");
 	res = f_readdir(&dirs, &finfo);
 	res = f_open(&file, "SDCard_K.txt", FA_OPEN_ALWAYS | FA_READ | FA_WRITE);
-	res = f_write(&file, WriteData, 40, (UINT *)&i);
+	res = f_write(&file, WriteData, strlen(WriteData), (UINT *)&i);
 	file.fptr = 0;
-	res = f_read(&file, ReadBuf, 100, (UINT *)&i);
+	res = f_read(&file, ReadBuf, strlen(ReadBuf), (UINT *)&i);
 
 	f_close(&file);
 	printf("%s\n", ReadBuf);
