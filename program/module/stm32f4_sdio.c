2f6f44c5f5531d2eed7a9d8950eefddbeeac80e6
Shengwen1997 <l1996812@gmail.com>
2014-01-08 00:33:37 +0800

    Fix NULL redefinition warning

--- program/module/stm32f4_sdio.c
+++ program/module/stm32f4_sdio.c
@@ -6,7 +6,7 @@
 /**
   * @brief  SDIO Static flags, TimeOut, FIFO Address
   */
-#define NULL 0
+//#define NULL 0
 #define SDIO_STATIC_FLAGS               ((u32)0x000005FF)
 #define SDIO_CMD0TIMEOUT                ((u32)0x00010000)
 
