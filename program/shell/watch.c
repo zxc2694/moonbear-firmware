1e0bbcf67191db2e214e2fd4436a18129f06619e
Shengwen Cheng <shengwen1997.tw@gmail.com>
2014-04-15 23:42:41 +0800

    Add the basic implementation of the watch command

--- program/shell/watch.c
+++ program/shell/watch.c
@@ -9,6 +9,17 @@ extern xTaskHandle watch_task_handle;
 char (*watch_arguments)[MAX_CMD_LEN];
 int watch_arg_cnt;
 
+global_t *find_variable(char *name) {
+	int i;
+	for(i = 0; i < system.var_count; i++) {
+		if(strcmp(name, system.variable[i].name) == 0)
+				return system.variable + i;
+	}
+
+	/* Can't find the variable */
+	return 0;
+}
+
 void watch_gui()
 {
 	while(1) {
@@ -26,6 +37,24 @@ void shell_watch(char parameter[][MAX_CMD_LEN], int par_cnt)
 {
 	watch_arguments = parameter;
 	watch_arg_cnt = par_cnt;	
+
+	/* Check for the passing arguements is exist */
+	int i;
+	for(i = 0; i < par_cnt; i++) {
+		if(i == 0 && strcmp(watch_arguments[i], "-gui") == 0)
+			continue;
+
+		if(i > 0 && strcmp(watch_arguments[i], "-gui") == 0) {
+			serial.printf("Error: -gui mode can only working independently!\n\r");
+			return;
+		} else if(find_variable(watch_arguments[i]) == 0) {
+			serial.printf("Error: Unknown variable!\n\r");
+			return;	
+		}
+	}
+
+	/* Prompt */
+	serial.printf("Refresh time: 1.0s\n\r");
 
 	/* Enable the watch task */
 	vTaskResume(watch_task_handle);
@@ -47,9 +76,22 @@ void shell_watch(char parameter[][MAX_CMD_LEN], int par_cnt)
 
 void watch_task()
 {
+	global_t *find_var;
+
 	while(1) {
-		if(strcmp(watch_arguments[1], "-gui")) {
+		if(strcmp(watch_arguments[0], "-gui") == 0) {
 			watch_gui();
+			continue;
 		}
+
+		int i;
+		for(i = 0; i < watch_arg_cnt; i++) {
+			find_var = find_variable(watch_arguments[i]);
+			serial.printf("%s : %d\n\r", watch_arguments[i], find_var->value);
+		}
+
+		serial.printf("<Delay 1 sec ...>\n\r");
+
+		vTaskDelay(1000);
 	}
 }
