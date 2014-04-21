4f467c9353917af4bb97a15ba0c55224b98ed0d2
Shengwen Cheng <shengwen1997.tw@gmail.com>
2014-04-21 22:59:59 +0800

    Create the prototype function of the binary transmit (USART version)

---
+++ program/system/communication.c
@@ -0,0 +1,12 @@
+#include <stddef.h>
+#include <stdlib.h>
+
+#include "QuadCopterConfig.h"
+
+void generate_package(IMU_package *package, uint8_t *buf)
+{
+}
+
+void send_package(uint8_t *buf)
+{
+}
