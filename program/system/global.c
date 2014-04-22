097ff90563df8465f4e032f12b4ec00846de5129
fboris <fantasyboris@gmail.com>
2014-04-22 20:20:18 +0800

    Change naming style(type more faster)

--- program/system/global.c
+++ program/system/global.c
@@ -1,31 +1,31 @@
 #include "QuadCopterConfig.h"
 
 global_t variable[] = {
-	[PWM1_CCR] = {.name = "PWM1"},
-	[PWM2_CCR] = {.name = "PWM2"},
-	[PWM3_CCR] = {.name = "PWM3"},
-	[PWM4_CCR] = {.name = "PWM4"},
-	[PWM5_CCR] = {.name = "PWM5"},
+	[PWM1_CCR] = {.name = "pwm1"},
+	[PWM2_CCR] = {.name = "pwm2"},
+	[PWM3_CCR] = {.name = "pwm3"},
+	[PWM4_CCR] = {.name = "pwm4"},
+	[PWM5_CCR] = {.name = "pwm5"},
 
 	[MOTOR1] = {.name = "motor1"},
 	[MOTOR2] = {.name = "motor2"},
 	[MOTOR3] = {.name = "motor3"},
 	[MOTOR4] = {.name = "motor4"},
 
-	[NO_RC_SIGNAL_MSG] = {.name = "RC.status"},
+	[NO_RC_SIGNAL_MSG] = {.name = "rc.status"},
 
-	[RC_EXP_PITCH] = {.name = "RC.pitch"},
-	[RC_EXP_ROLL] = {.name = "RC.roll"},
-	[RC_EXP_YAW] = {.name = "RC.yaw"},
-	[RC_EXP_THR] = {.name = "RC.throttle"},
+	[RC_EXP_PITCH] = {.name = "rc.pitch"},
+	[RC_EXP_ROLL] = {.name = "rc.roll"},
+	[RC_EXP_YAW] = {.name = "rc.yaw"},
+	[RC_EXP_THR] = {.name = "rc.throttle"},
 
-	[TRUE_PITCH] = {.name = "IMU.pitch"},
-	[TRUE_ROLL] = {.name = "IMU.roll"},
-	[TRUE_YAW] = {.name = "IMU.yaw"},
+	[TRUE_PITCH] = {.name = "imu.pitch"},
+	[TRUE_ROLL] = {.name = "imu.roll"},
+	[TRUE_YAW] = {.name = "imu.yaw"},
 
-	[PID_PITCH] = {.name = "PID.pitch"},
-	[PID_ROLL] = {.name = "PID.roll"},
-	[PID_YAW] = {.name = "PID.yaw"}
+	[PID_PITCH] = {.name = "pid.pitch"},
+	[PID_ROLL] = {.name = "pid.roll"},
+	[PID_YAW] = {.name = "pid.yaw"}
 };
 
 system_t system = {
