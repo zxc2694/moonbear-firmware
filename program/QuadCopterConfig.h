c3d37da2f011a07f0ba11610f39aa9b54cb9cff7
zxc2694 <zxc2694zxc2694@gmail.com>
2015-07-08 11:56:40 +0800

    DEMO ((balance control of Quadrotor-Motorcycle

--- program/QuadCopterConfig.h
+++ program/QuadCopterConfig.h
@@ -6,13 +6,13 @@
 /* Hardware environments */
 #define configMotor				1       //if you no connect motor -> set 0. 
 /* Status report functions */
-#define configSTATUS_SHELL		1
+#define configSTATUS_SHELL		0
 #define configSTATUS_GET_MOTORS	0
 #define configSTATUS_GET_ROLL_PITCH	0
-#define configSTATUS_GET_ROLL_PITCH_MOTORS 0
+#define configSTATUS_GET_ROLL_PITCH_MOTORS 1
 
 /* Quadrotor-Motorcycle mode */
-#define QuadrotorMotorcycle 0
+#define QuadrotorMotorcycle 1
 
 /* Includes */
 //FreeRTOS
